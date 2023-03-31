c0f473502af6f2e4848764a5b9723424f0cd5cac
Diegodumar <diegoalvess.mkt@gmail.com>
2023-03-30 19:28:35 -0500

    update 2 / 30-03

---
+++ script.js
@@ -0,0 +1,9 @@
+function clickMenu(){
+    if  (menupaleta.style.display == 'block'){
+        menupaleta.style.display = 'none'} else {
+            menupaleta.style.display = 'block'
+        }
+    }
+
+// CARROSSEL___________________//
+
